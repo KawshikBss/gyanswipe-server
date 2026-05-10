82c43c002f9c0bd00746c81a32e6aefd9a89887b
KawshikBss <kawshikbss@gmail.com>
2026-05-10 13:37:52 +0600

    modified feed controller

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,15 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Content;
+use App\Models\UserActivity;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $contents = Content::where('is_published', true)
             ->orderBy('published_at', 'desc')
             ->paginate(3);
+        $contentIds = $contents->pluck('id');
+        $activities = UserActivity::query()
+            ->where('device_id', $request->device_id)
+            ->whereIn('content_id', $contentIds)
+            ->get();
+        $grouped = $activities->groupBy('content_id');
+        $contents->getCollection()->transform(
+            function ($content) use ($grouped) {
+
+                $activitySet =
+                    collect($grouped[$content->id] ?? [])
+                    ->pluck('action');
+
+                $content->is_liked =
+                    $activitySet->contains('like');
+
+                $content->is_saved =
+                    $activitySet->contains('save');
+
+                $content->is_viewed =
+                    $activitySet->contains('view');
+
+                return $content;
+            }
+        );
         return response()->json($contents);
     }
 }
