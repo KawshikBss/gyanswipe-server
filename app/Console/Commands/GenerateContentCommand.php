653b33a96432b7530bc687cf8b2dfd4433ae6c0b
KawshikBss <kawshikbss@gmail.com>
2026-05-14 20:27:04 +0600

    created command for generating content

---
+++ app/Console/Commands/GenerateContentCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Category;
+use App\Services\ContentGenerationService;
+use Illuminate\Console\Command;
+
+class GenerateContentCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'content:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(
+        ContentGenerationService $service
+    ) {
+
+        $categories = Category::get();
+        foreach ($categories as $category) {
+            $content =
+                $service->generateFromCategory($category);
+
+            $this->info(
+                "Generated content: {$content->title}"
+            );
+        }
+    }
+}
