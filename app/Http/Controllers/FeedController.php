81309be5fe4800813a05e96267c838831f42eb75
KawshikBss <kawshikbss@gmail.com>
2026-05-16 14:13:08 +0600

    added search

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -4,13 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Content;
 use App\Models\UserActivity;
+use App\Models\UserPreferredCategory;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
     public function index(Request $request)
     {
-        $contents = Content::where('is_published', true)
+        $userPreferredCategories = UserPreferredCategory::where('device_id', $request->device_id)->pluck('category_id')->toArray();
+        $contents = Content::where('is_published', true)->when(
+            count($userPreferredCategories),
+            fn($q) =>
+            $q->whereIn(
+                'category_id',
+                $userPreferredCategories
+            )
+        )
             ->orderBy('published_at', 'desc')
             ->paginate(3);
         $contentIds = $contents->pluck('id');
@@ -47,7 +56,54 @@ class FeedController extends Controller
             ->where('device_id', $request->device_id)
             ->where('action', 'save')->get();
         $contentIds = $activities->pluck('content_id');
-        $contents = Content::whereIn('id', $contentIds)->paginate(1);
+        $contents = Content::whereIn('id', $contentIds)->paginate(5);
+        $activities = UserActivity::query()
+            ->where('device_id', $request->device_id)
+            ->whereIn('content_id', $contentIds)
+            ->get();
+        $grouped = $activities->groupBy('content_id');
+        $contents->getCollection()->transform(
+            function ($content) use ($grouped) {
+
+                $activitySet =
+                    collect($grouped[$content->id] ?? [])
+                    ->pluck('action');
+
+                $content->is_liked =
+                    $activitySet->contains('like');
+
+                $content->is_saved =
+                    $activitySet->contains('save');
+
+                $content->is_viewed =
+                    $activitySet->contains('view');
+
+                return $content;
+            }
+        );
+        return response()->json($contents);
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $categories = $request->input('categories', []);
+        if (is_string($categories)) {
+            $categories = explode(',', $categories);
+        }
+        $contents = Content::where('is_published', true)->when(
+            count($categories),
+            fn($q) =>
+            $q->whereIn(
+                'category_id',
+                $categories
+            )
+        )->where(function ($q) use ($query) {
+            $q->where('title', 'like', "%$query%");
+        })
+            ->orderBy('published_at', 'desc')
+            ->paginate(5);
+        $contentIds = $contents->pluck('id');
         $activities = UserActivity::query()
             ->where('device_id', $request->device_id)
             ->whereIn('content_id', $contentIds)
